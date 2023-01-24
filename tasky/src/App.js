26cf7f25e399356d01720d93cea1562c5dec8aa5
Mark <20025214@wit.ie>
2023-01-24 10:51:39 +0000

    Completed Week 2 Lab A

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -5,7 +5,7 @@ import Task from './components/Task';
 function App() {
   return (
     <div className="container">
-      <h1>Tasky</h1>
+      <h1>Tasky e1</h1>
       <Task title="Dishes" deadline="Today" description=" Empty dishwasher" />
   
       <Task title="Laundry" deadline="Tomorrow" description= "Fold laundry and put away" />
